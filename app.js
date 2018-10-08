ea9ec94f853770f38f422c4501a8cea7903d8083
Joshmossass <joshmsosso@gmail.com>
2018-10-08 02:23:31 -0500

    initial experimentation

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-var express = require("express")
-var app = express();
+const express = require("express")
+const app = express();
+const bodyParser = require("body-parser");
+const igdb = require('igdb-api-node').default;
+require('dotenv').config()
+
+const client = igdb(process.env.apiKey);
+
+app.set("view engine", "ejs");
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.send("hello world")
+    client.games({
+        limit: 10,
+        fields: "*",
+        order: "popularity:desc"
+    })
+    .then(response => {
+        res.render("home", {data: response.body})
+    }).catch(error => {
+        throw error
+    })
+});
+
+app.get("/games/:id", function(req, res){
+    var gameid = req.params.id
+    client.games({
+        ids: [gameid],
+        fields: '*'
+    })
+    .then(response => {
+        console.log(response)
+        res.render("gameSingle", {data: response.body})
+    })
+    .catch(error => {
+        throw error
+    })
 })
 
 app.listen(8000, function(){
