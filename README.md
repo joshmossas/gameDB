81719b467e366ed550c5378f38beacd497dba5ce
Joshmossass <joshmsosso@gmail.com>
2018-10-09 02:18:54 -0500

    add search route use url parameters for search and sorting terms

---
+++ README.md
@@ -0,0 +1,5 @@
+# GameDB - An Example Video Game Website using IGDB API
+
+Simple project I made to familiarize myself with [ExpressJS](https://expressjs.com/). The site uses the [IGDB API](https://igdb.com) to get game information and populate the website. 
+
+Bulma CSS framework was used for styling.
--- app.js
+++ app.js
@@ -11,17 +11,59 @@ app.use(express.static('static'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    client.games({
-        limit: 10,
-        fields: "*",
-        order: "popularity:desc"
-    })
+    res.render("home")
+});
+
+app.get("/games", function(req,res) {
+        let sort = 'popularity:desc'
+        let page = 1
+        let offset = 0
+        if(req.query.sort) {
+            sort = req.query.sort
+        }
+        if(req.query.page) {
+            page = req.query.page - 1
+            offset = 50 * page
+        }
+        client.games({
+            limit: 50,
+            offset: offset,
+            order: sort,
+            fields: ["name", "id", "cover", "aggregated_rating", "rating", "genres"],
+        })
+        .then(response => {
+            res.render("gameList", {data: response})
+        })
+        .catch(error => {
+            throw error
+        })
+})
+
+app.get("/games/search", function(req, res){
+    let search = req.query.query
+    let sort = req.query.sort
+    let page = req.query.page
+    let pageNum = 1
+    let settings = {limit: 20, offset: 0, fields: '*'}
+    if(search) {
+        settings.search = search
+    }
+    if(sort) {
+        settings.order = sort
+    }
+    if(page) {
+        let pageCalc = Number(page) - 1
+        pageNum = page
+        settings.offset = 20 * pageCalc
+    }
+    client.games(settings)
     .then(response => {
-        res.render("home", {data: response.body})
-    }).catch(error => {
+        res.render("search", {data: response, searchQuery: search, sortOrder: sort, offset: settings.offset, page: pageNum})
+    })
+    .catch(error => {
         throw error
     })
-});
+})
 
 app.get("/games/:id", function(req, res){
     var gameid = req.params.id
@@ -30,8 +72,26 @@ app.get("/games/:id", function(req, res){
         fields: '*'
     })
     .then(response => {
-        console.log(response)
-        res.render("gameSingle", {data: response.body})
+        let genres = response.body[0].genres
+        let genreData
+        let data = response.body[0]
+        if (genres) {
+            client.genres({
+                ids: genres,
+                fields: ["name", "id"]
+            })
+            .then(response => {
+                genreData = response.body
+                res.render("gameSingle", {data: data, genres: genreData})
+            })
+            .catch(error => {
+                throw error
+            })    
+        }
+        else {
+            res.render("gameSingle", {data: data, genres: ''})
+        }
+        
     })
     .catch(error => {
         throw error
@@ -39,5 +99,5 @@ app.get("/games/:id", function(req, res){
 })
 
 app.listen(8000, function(){
-    console.log("server running. Project available at http://localhost:8000")
+    console.log("Server running.\nView project at http://localhost:8000")
 })
